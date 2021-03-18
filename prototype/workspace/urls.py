2f4b707bf1cc341d5ace269aa7eb1c2dc08558f2
Kunhee Ha <kunheeha@gmail.com>
2021-03-18 15:20:38 +0000

    dashboard ui prototype

--- prototype/workspace/urls.py
+++ prototype/workspace/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from .views import DashboardListView
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('dashboard/', DashboardListView.as_view(), name='user-dashboard'),
 ]
--- prototype/workspace/views.py
+++ prototype/workspace/views.py
@@ -1,5 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView
+from .models import Workspace
 
 
 def home(request):
-    return render(request, 'workspace/home.html', {})
+    return render(request, 'workspace/home.html')
+
+
+class DashboardListView(LoginRequiredMixin, ListView):
+    model = Workspace
+    template_name = 'workspace/dashboard.html'
+    context_object_name = 'workspaces'
+
+    def get_queryset(self):
+        # dashuser = get_object_or_404(
+        #     User, username=self.kwargs.get('username'))
+        dashuser = self.request.user
+        return Workspace.objects.filter(users=dashuser)
