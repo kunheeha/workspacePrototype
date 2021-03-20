4ecf57e62950f7fc192816648cf8cb9d139f0bf7
Kunhee Ha <kunheeha@gmail.com>
2021-03-20 12:42:06 +0000

    todoitems

--- prototype/workspace/views.py
+++ prototype/workspace/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views import View
 from django.views.generic import ListView
-from .models import Workspace, TodoItem
+from .models import Workspace, TodoItem, Folder
 
 
 def home(request):
@@ -21,13 +21,21 @@ class DashboardListView(LoginRequiredMixin, ListView):
         return Workspace.objects.filter(users=dashuser)
 
 
+@login_required
 def workspace(request, *args, **kwargs):
     currentWorkspace = Workspace.objects.filter(
         id=kwargs['workspaceid']).first()
     todoitems = TodoItem.objects.filter(workspace=currentWorkspace)
+    folders = Folder.objects.filter(workspace=currentWorkspace)
     authorised_users = User.objects.filter(workspace=currentWorkspace)
 
+    context = {
+        'workspace': currentWorkspace,
+        'todoitems': todoitems,
+        'folders': folders
+    }
+
     if request.user in authorised_users:
-        return render(request, 'workspace/workspace.html', {'workspace': currentWorkspace, 'todoitems': todoitems})
+        return render(request, 'workspace/workspace.html', context)
 
     return render(request, 'workspace/deniedaccess.html')
