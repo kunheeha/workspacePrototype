ef579e7412fc6b1154c58b2d64198565b91b1db8
Kunhee Ha <kunheeha@gmail.com>
2021-03-21 19:21:33 +0000

    add and delete todo

--- prototype/workspace/urls.py
+++ prototype/workspace/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('dashboard/', DashboardListView.as_view(), name='user-dashboard'),
     path('workspace/<int:workspaceid>/', views.workspace, name='user-workspace'),
+    path('workspace/<int:workspaceid>/<int:folderid>',
+         views.folder, name='workspace-folder'),
+    path('deletetodo/<int:workspaceid>/<int:todoid>', views.deleteTodo, name='delete-todo'),
 ]
--- prototype/workspace/views.py
+++ prototype/workspace/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
@@ -23,8 +23,8 @@ class DashboardListView(LoginRequiredMixin, ListView):
 
 @login_required
 def workspace(request, *args, **kwargs):
-    currentWorkspace = Workspace.objects.filter(
-        id=kwargs['workspaceid']).first()
+    workspaceid = kwargs['workspaceid']
+    currentWorkspace = Workspace.objects.filter(id=workspaceid).first()
     todoitems = TodoItem.objects.filter(workspace=currentWorkspace)
     folders = Folder.objects.filter(workspace=currentWorkspace)
     authorised_users = User.objects.filter(workspace=currentWorkspace)
@@ -35,7 +35,39 @@ def workspace(request, *args, **kwargs):
         'folders': folders
     }
 
+    if request.method == 'POST':
+        if 'addTodo' in request.POST:
+            name = request.POST['todoName']
+            newTodo = TodoItem(name=name, workspace=currentWorkspace)
+            newTodo.save()
+            return redirect('user-workspace', workspaceid=workspaceid)
+
     if request.user in authorised_users:
         return render(request, 'workspace/workspace.html', context)
+
+    return render(request, 'workspace/deniedaccess.html')
+
+
+def deleteTodo(request, *args, **kwargs):
+    workspaceid = kwargs['workspaceid']
+    todoId = kwargs['todoid']
+    todoitem = TodoItem.objects.filter(id=todoId).first()
+    todoitem.delete()
+    return redirect('user-workspace', workspaceid=workspaceid)
+
+
+@login_required
+def folder(request, *args, **kwargs):
+    currentWorkspace = Workspace.objects.filter(
+        id=kwargs['workspaceid']).first()
+    currentFolder = Folder.objects.filter(id=kwargs['folderid']).first()
+    authorised_users = User.objects.filter(workspace=currentWorkspace)
+
+    context = {
+        'workspace': currentWorkspace,
+        'folder': currentFolder
+    }
+    if request.user in authorised_users:
+        return render(request, 'workspace/folder.html', context)
 
     return render(request, 'workspace/deniedaccess.html')
